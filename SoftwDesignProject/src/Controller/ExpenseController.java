cce365833fb7032e29d94a71ec8c6d748b3b0ce1
Arvo cant <arvo.cant@student.uantwerpen.be>
2023-11-21 11:16:39 +0100

    Commit mvc first

---
+++ SoftwDesignProject/src/Controller/ExpenseController.java
@@ -0,0 +1,9 @@
+package Controller;
+
+import Expense.ExpenseDescription;
+import Expense.*;
+import Person.Person;
+
+public class ExpenseController implements Controller{
+
+}
---
+++ SoftwDesignProject/src/Model/ExpenseModel.java
@@ -0,0 +1,13 @@
+package Model;
+
+import Expense.ExpenseDescription;
+import Person.Person;
+
+public class ExpenseModel {
+
+    private String id;
+    private double amount;
+    private Person paidBy;
+    //private List<Split> splits;
+    private ExpenseDescription description;
+}
